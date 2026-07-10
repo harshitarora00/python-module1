8a397222fa051a51d479c11bd81cef55302483ec
harshitarora00 <aroraharshit015@gmail.com>
2026-07-10 23:11:36 +0530

    power calculator

---
+++ powercalc.py
@@ -0,0 +1,8 @@
+# Program to calculate the n-th power of a given number
+
+number = float(input("Enter the number: "))
+n = int(input("Enter the power (n): "))
+
+result = number ** n
+
+print("The", n, "power of", number, "is", result)
