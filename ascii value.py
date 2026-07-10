29866966fea7eb5752b462f53bc07694346c67fe
harshitarora00 <aroraharshit015@gmail.com>
2026-07-10 19:13:52 +0530

    ascii value

---
+++ ascii value.py
@@ -0,0 +1,9 @@
+# ASCII Value Checker
+
+character = input("Enter a character: ")
+
+if len(character) == 1:
+    ascii_value = ord(character)
+    print("The ASCII value of", character, "is", ascii_value)
+else:
+    print("Please enter only one character.")
