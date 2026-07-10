27ccc29e04f9a931d217b01bcbe3267af711fd27
harshitarora00 <aroraharshit015@gmail.com>
2026-07-10 17:11:56 +0530

    summer times

---
+++ summertimes.py
@@ -0,0 +1,8 @@
+# Program to check if the temperature is suitable for wearing light clothes
+
+temperature = float(input("Enter the temperature (in °C): "))
+
+if temperature >= 25:
+    print("The temperature is suitable for wearing light and soft clothes.")
+else:
+    print("The temperature is not suitable for wearing light clothes. Wear warm clothes.")
